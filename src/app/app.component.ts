2124629861d90e812bfed4130e14e0d541f9ce2c
Luis Fernando Nestares Valverde <68039881+luisfernando1901@users.noreply.github.com>
2021-04-06 16:30:43 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Track } from 'ngx-audio-player'; 
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,98 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'distimia-album';
+  title = 'distimia';
+  cover_src: string = '../assets/cover/distimia.jpeg'
+  msaapDisplayTitle = true;
+  msaapDisplayPlayList = true;
+  msaapPageSizeOptions = [3];
+  msaapDisplayVolumeControls = true;
+  msaapDisplayRepeatControls = true;
+  msaapDisplayArtist = false;
+  msaapDisplayDuration = false;
+  msaapDisablePositionSlider = false;
+  msaapPlaylist: Track[] = [
+    {
+      link: "../assets/songs/Intro.mp3",
+      title: "Intro"
+    },
+    {
+      link: "../assets/songs/Encerrado.mp3",
+      title: "Encerrado"
+    },
+    {
+      link: "../assets/songs/Cuerdos.mp3",
+      title: "Cuerdos"
+    },
+    {
+      link: "../assets/songs/Corto 1.mp3",
+      title: "Corto 1"
+    },
+    {
+      link: "../assets/songs/Casi nunca.mp3",
+      title: "Casi nunca"
+    },
+    {
+      link: "../assets/songs/Expuestos.mp3",
+      title: "Expuestos"
+    },
+    {
+      link: "../assets/songs/Corto 2 pirata.mp3",
+      title: "Corto 2 pirata"
+    },
+    {
+      link: "../assets/songs/Fama.mp3",
+      title: "Fama"
+    },
+    {
+      link: "../assets/songs/Ganar o perder.mp3",
+      title: "Ganar o perder"
+    },
+    {
+      link: "../assets/songs/Desampáramo.mp3",
+      title: "Desampáramo"
+    }
+  ];
+  audioList: object[] = [
+    {
+      url: "../assets/songs/Intro.mp3",
+      title: "Intro"
+    },
+    {
+      url: "../assets/songs/Encerrado.mp3",
+      title: "Encerrado"
+    },
+    {
+      url: "../assets/songs/Cuerdos.mp3",
+      title: "Cuerdos"
+    },
+    {
+      url: "../assets/songs/Corto 1.mp3",
+      title: "Corto 1"
+    },
+    {
+      url: "../assets/songs/Casi nunca.mp3",
+      title: "Casi nunca"
+    },
+    {
+      url: "../assets/songs/Expuestos.mp3",
+      title: "Expuestos"
+    },
+    {
+      url: "../assets/songs/Corto 2 pirata.mp3",
+      title: "Corto 2 pirata"
+    },
+    {
+      url: "../assets/songs/Fama.mp3",
+      title: "Fama"
+    },
+    {
+      url: "../assets/songs/Ganar o perder.mp3",
+      title: "Ganar o perder"
+    },
+    {
+      url: "../assets/songs/Desampáramo.mp3",
+      title: "Desampáramo"
+    },
+  ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { NgxAudioPlayerModule } from 'ngx-audio-player';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatTableModule} from '@angular/material/table';
+import {MatPaginatorModule} from '@angular/material/paginator';
+import {MatSliderModule} from '@angular/material/slider';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatExpansionModule} from '@angular/material/expansion';
+import {MatCardModule} from '@angular/material/card';
+import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    NgxAudioPlayerModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSliderModule,
+    MatFormFieldModule,
+    MatExpansionModule,
+    MatCardModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
